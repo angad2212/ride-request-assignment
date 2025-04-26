f0e2ee20e64b82aa61a3b28b296e98f7df662936
Angad Suryavanshi <135613657+angad2212@users.noreply.github.com>
2025-04-27 01:19:55 +0530

    initial code update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
 //main
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b){
+    int result = 0;
+    for(int i = a; i <= b; i++){
+        result += i;
+    }
+    return result;
+}
+
+int main(){
+    int x = 1;
+    int y = 4;
+    int ans = sum(x,y);
+    cout << ans;
+}
